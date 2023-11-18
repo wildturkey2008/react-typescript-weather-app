618a1287c0ecdc32fdf679d903e8a56a6889aeb1
Kazumi Miura <bluesky-dev@aoisora.red>
2023-11-18 18:03:26 +0900

    program be completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
+import Title from "./components/Title";
+import Form from "./components/Forms";
+import Results from "./components/Results";
+import { useState } from "react";
+import "./App.css";
 
-import './App.css';
+type ResultsStateType = {
+  country: string;
+  cityName: string;
+  temperature: string;
+  conditionText: string;
+  icon: string;
+};
 
 function App() {
-  return (
-    <div className="App">
+  const [city, setCity] = useState<string>("");
+  const [results, setResults] = useState<ResultsStateType>({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: "",
+  });
 
+  const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetch(
+      `http://api.weatherapi.com/v1/current.json?key=4c057edfe9874d1dafa44716231811&q=${city}&aqi=no`
+    )
+      .then((res) => res.json())
+      .then((data) =>
+        setResults({
+          country: data.location.country,
+          cityName: data.location.name,
+          temperature: data.current.temp_c,
+          conditionText: data.current.condition.text,
+          icon: data.current.condition.icon,
+        })
+      );
+  };
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <Title />
+        <Form setCity={setCity} getWeather={getWeather} />
+        <Results results={results} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Results.tsx
@@ -0,0 +1,34 @@
+// Results.tsx
+
+type ResultsPropsType = {
+  results: {
+    country: string;
+    cityName: string;
+    temperature: string;
+    conditionText: string;
+    icon: string;
+  };
+};
+
+const Results = (props: ResultsPropsType) => {
+  return (
+    <div>
+      {props.results.country && <div>{props.results.country}</div>}
+      {props.results.cityName && <div>{props.results.cityName}</div>}
+      {props.results.temperature && (
+        <div>
+          {props.results.temperature}
+          <span>°C</span>{" "}
+        </div>
+      )}
+      {props.results.conditionText && (
+        <div>
+          <img src={props.results.icon} alt="icon" />
+          <span>{props.results.conditionText}</span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Results;
